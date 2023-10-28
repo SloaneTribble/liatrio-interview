0871af18540906188af037c6a011ca3e9c72a18a
SloaneTribble <sloanetribble@gmail.com>
2023-10-28 14:16:32 -0700

    Create application exposing an HTTP API endpoint

---
+++ endpoint/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+// create an instance of express application
+const app = express();
+
+// TODO: Update to accommodate github action test?
+const port = 3000; 
+
+const message = "My name is Sloane."
+
+// define a route that returns a message with a timestamp.
+app.get('/', (req, res) => {
+
+  // dynamically generate a Unix style timestamp
+  const timestamp = Date.now();
+
+  const response = {
+    message: message,
+    timestamp: timestamp
+  }
+  res.send(response);
+});
+
+// start the server;
+// express application will listen for incoming http requests on the given port
+// no hostname specified, so accept requests on all available network interfaces -- 
+// TODO: Any change required to accommodate github action test? 
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
